282f25edb76958a67de73d310acccbacb4f2d97f
fab-du <batlab.official@gmail.com>
2016-03-06 21:15:24 +0100

    add proper exception and static field

--- src/main/java/de/crypto/AESCrypto.java
+++ src/main/java/de/crypto/AESCrypto.java
@@ -19,116 +19,102 @@ import de.cryptone.utils.Helper;
 
 public class AESCrypto {
 
-	
-	public KeySym generateKey(){
-		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+    public final static int SALT_LENGHT = 32; 
 
-			try {
-				KeyGenerator keygen = null;
-				KeySym result = new KeySym();
-				byte[] salt = new byte[32];
-				new SecureRandom();
-				final SecureRandom r =  SecureRandom.getInstanceStrong();
-				r.nextBytes(salt);
-				keygen = KeyGenerator.getInstance("AES");
-				keygen.init(128, r );
-				Key k = keygen.generateKey();
-				result.setSymkey( Helper.encode( k.getEncoded()));
-				result.setSalt( Helper.encode(salt));
-				return result;
-			} catch (NoSuchAlgorithmException e) {
-				return null;
-			}
+    public final static int KEY_LENGHT = 128; 
+
+    public final static String SYM_KEY_ALGO  = "AES";
+
+    public final static String SYM_CIPHER_ALGO = "AES/ECB/PKCS7Padding";
+
+    public final static String EXC_MESS_NULL = "no arguments provided:";
+	
+	public KeySym generateKey() throws NoSuchAlgorithmException {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        KeyGenerator keygen = null;
+        KeySym result = new KeySym();
+        byte[] salt = new byte[ SALT_LENGHT ];
+        new SecureRandom();
+        final SecureRandom r =  SecureRandom.getInstanceStrong();
+        r.nextBytes(salt);
+        keygen = KeyGenerator.getInstance( SYM_KEY_ALGO );
+        keygen.init(KEY_LENGHT, r );
+        Key k = keygen.generateKey();
+        result.setSymkey( Helper.encode( k.getEncoded()));
+        result.setSalt( Helper.encode(salt));
+        return result;
 	}
 	
 	Key symkeyFromString(String key) {
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		byte[] bytes = key.getBytes();
 		byte[] keybytes = Base64.getDecoder().decode(bytes);
-		Key result = new SecretKeySpec(keybytes, 0, keybytes.length, "AES");
+		Key result = new SecretKeySpec( keybytes, 0, keybytes.length, SYM_KEY_ALGO );
 		return result;
 	}
 
-	public  String decrypt( final String secretkey, final String message ) {
-		 Cipher cipher = null;
-		 String clearText = null;
-		 byte[] stringBytes = null;
-
+	public  String decrypt( final String secretkey, final String message ) throws Exception {
 		Security.addProvider( new org.bouncycastle.jce.provider.BouncyCastleProvider());
+		byte[] stringBytes = null;
+
+         if ( secretkey == null || message == null ) 
+             throw new Exception( EXC_MESS_NULL + "public  String decrypt( final String secretkey, final String message )" );
 
 		 Key key = this.symkeyFromString(secretkey);
-		 if( key == null ) return null;
-		 
-		 try {
-			  cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
-		      cipher.init(Cipher.DECRYPT_MODE, key);
-		      byte[] raw = Base64.getDecoder().decode(message);
-		      stringBytes = cipher.doFinal(raw);
-		      clearText= new String(stringBytes, "UTF8");
-			} catch (Exception e) {
-				return null;
-			} 
-
-		return clearText;
+         Cipher cipher = Cipher.getInstance( SYM_CIPHER_ALGO , "BC");
+         cipher.init(Cipher.DECRYPT_MODE, key);
+         byte[] raw = Base64.getDecoder().decode(message);
+         stringBytes = cipher.doFinal(raw);
+         String clearText= new String(stringBytes, "UTF8");
+		 return clearText;
 	}
 
-	public String encrypt(final String secretkey, final String message) {
+	public String encrypt(final String secretkey, final String message) throws Exception {
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+        if ( secretKey == null || message == null )
+             throw new Exception( EXC_MESS_NULL + "public  String encrypt( final String secretkey, final String message )" );
+
 		Cipher cipher = null;
-		byte[] raw = null;
 
 		Key key = this.symkeyFromString(secretkey);
 
-		if (key == null)
-			return null;
+        Cipher cipher = Cipher.getInstance(SYM_CIPHER_ALGO, "BC");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] stringBytes = message.getBytes();
+        byte[] raw = cipher.doFinal(stringBytes);
 
-		try {
-			cipher = Cipher.getInstance("AES/ECB/PKCS7Padding", "BC");
-			cipher.init(Cipher.ENCRYPT_MODE, key);
-			byte[] stringBytes = message.getBytes();
-			raw = cipher.doFinal(stringBytes);
+		if (raw == null) 
+            throw new Exception("Encryption goes wrong");
 
-		} catch (Exception e) {
-			System.out.println(e.toString());
-			return null;
-		}
-
-		if (raw == null)
-			return null;
 		return Base64.getEncoder().encodeToString(raw);
 	}
 	
-	private void chifferImp( final String secretkey, File file, int modus){
+	private void chifferImp( final String secretkey, File file, int modus) throws Exception{
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		
+        if ( secretkey == null || file == null || modus == null )
+            throw new Exception( EXC_MESS_NULL + "private void chifferImp( final String secretkey, File file, int modus)" );
+
 		Cipher cipher = null;
 		Key key = this.symkeyFromString(secretkey);
-
-		if (key == null) return;
 		
-		try {
-			cipher = Cipher.getInstance("AES/ECB/PKCS5Padding", "BC");
-			cipher.init(modus, key);
+		Cipher cipher = Cipher.getInstance( SYM_CIPHER_ALGO, "BC");
+		cipher.init(modus, key);
 
-			BufferedInputStream bis = new BufferedInputStream( new FileInputStream(file));
-			File result = new File(file.getName() + ".enc");
-			BufferedOutputStream bos = new BufferedOutputStream( new FileOutputStream(result));
+        BufferedInputStream bis = new BufferedInputStream( new FileInputStream(file));
+        File result = new File(file.getName() + ".enc");
+        BufferedOutputStream bos = new BufferedOutputStream( new FileOutputStream(result));
 			
-			byte[] readed = new byte[64];
+        byte[] readed = new byte[64];
 			
-			while( (bis.read(readed)) != -1 ){
-				byte[] encrypted = cipher.update(readed, 0, 64);
-				bos.write(encrypted);
-			}
+        while( (bis.read(readed)) != -1 ){
+            byte[] encrypted = cipher.update(readed, 0, 64);
+            bos.write(encrypted);
+        }
 			
-			bis.close();
-			bos.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			return;
-		}
-
+        bis.close();
+        bos.close();
 	}
 
 	public void decrypt(final String secretkey, File file) {
